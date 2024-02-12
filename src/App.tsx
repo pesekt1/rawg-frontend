562f552c022125f68978c00712cbb6ef085bafd7
Tomas Pesek <pesekt@gmail.com>
2024-02-12 09:58:44 +0100

    added responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem bg="orange.300" gridArea={"nav"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink.300" gridArea={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green.300" gridArea={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
