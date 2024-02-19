9ecaf8b09f423e4a8a847d14620e9f83eff35821
Tomas Pesek <pesekt@gmail.com>
2024-02-19 12:12:34 +0100

    implemented abort controller

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,23 +1,30 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
 interface Response<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(path: string) => {
+const useData = <T>(endpoint: string) => {
   const [data, setData] = useState<T[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
     setIsLoading(true);
     apiClient
-      .get<Response<T>>(path)
+      .get<Response<T>>(endpoint, { signal: controller.signal })
       .then((res) => setData(res.data.results))
-      .catch((error) => setError(error))
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error);
+      })
       .finally(() => setIsLoading(false));
+
+    return () => controller.abort();
   }, []);
 
   return { data, isLoading, error };
