77ab24c40d99c7466a3b5db04c066da437f18879
Tomas Pesek <pesekt@gmail.com>
2024-02-19 09:30:19 +0100

    added icon list for GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconsList from "./PlatformIconsList";
 
 interface Props {
   game: Game;
@@ -14,6 +15,9 @@ const GameCard = ({ game }: Props) => {
         borderRadius="10"
       ></Image>
       <CardBody>
+        <PlatformIconsList
+          platforms={game.parent_platforms.map((pp) => pp.platform)}
+        />
         <Heading fontSize="2x1">{game.name}</Heading>
       </CardBody>
     </Card>
