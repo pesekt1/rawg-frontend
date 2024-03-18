1832ae3e3a6d0f972d76ce5a2539d9a747c9ba9f
Tomas Pesek <pesekt@gmail.com>
2024-03-18 10:38:02 +0100

    implemented games search input

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -30,7 +31,9 @@ function App() {
       }}
     >
       <GridItem gridArea={"nav"}>
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem gridArea={"aside"} paddingX={5}>
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,34 @@
+import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
+
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSearch(ref.current?.value || "");
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <BsSearch />
+        </InputLeftElement>
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
+  );
+};
+
+export default SearchInput;
